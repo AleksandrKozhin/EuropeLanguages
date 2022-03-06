1d4fcd067a2c954c2d8de7cdd089097f6adda59b
Aleksandr Kozhin <aleksandr.kozhin.v@gmail.com>
2022-03-06 22:16:50 +0200

    Created the method to map CountryResponse to Country

--- app/src/main/java/lt/alexdevstudio/europelanguages/data/model/CountryResponse.kt
+++ app/src/main/java/lt/alexdevstudio/europelanguages/data/model/CountryResponse.kt
@@ -1,7 +1,16 @@
 package lt.alexdevstudio.europelanguages.data.model
 
+import lt.alexdevstudio.europelanguages.domain.model.Country
+
 data class CountryResponse(
     val capitalInfo: CapitalInfo,
     val languages: Map<String, String>,
     val name: Name
-)
+) {
+    fun toCountry(): Country = Country(
+        name = name.common,
+        languages = languages,
+        latitude = capitalInfo.latlng[0],
+        longitude = capitalInfo.latlng[1]
+    )
+}
