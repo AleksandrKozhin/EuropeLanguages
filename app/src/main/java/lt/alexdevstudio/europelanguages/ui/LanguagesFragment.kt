600020cddb47d378992eb8db04c3cf0ba68d7e7c
Aleksandr Kozhin <aleksandr.kozhin.v@gmail.com>
2022-03-06 23:03:25 +0200

    Binding views in LanguagesFragment

--- app/src/main/java/lt/alexdevstudio/europelanguages/ui/LanguagesFragment.kt
+++ app/src/main/java/lt/alexdevstudio/europelanguages/ui/LanguagesFragment.kt
@@ -1,6 +1,33 @@
 package lt.alexdevstudio.europelanguages.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import lt.alexdevstudio.europelanguages.R
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import lt.alexdevstudio.europelanguages.databinding.FragmentLanguagesBinding
 
-class LanguagesFragment : Fragment(R.layout.fragment_languages)
+@AndroidEntryPoint
+class LanguagesFragment : Fragment() {
+    private val viewModel: LanguagesViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentLanguagesBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        binding.rvCountries.adapter = LanguagesAdapter()
+
+        binding.swipeContainer.setOnRefreshListener {
+            viewModel.getCountriesByRegion()
+            binding.swipeContainer.isRefreshing = false
+        }
+
+        return binding.root;
+    }
+}
