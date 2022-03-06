b468f3c7fd1fdf4a0b490f2fa4ff06b06da4b761
Aleksandr Kozhin <aleksandr.kozhin.v@gmail.com>
2022-03-06 22:44:21 +0200

    Created LanguagesFragment with layout

---
+++ app/src/main/java/lt/alexdevstudio/europelanguages/ui/LanguagesFragment.kt
@@ -0,0 +1,6 @@
+package lt.alexdevstudio.europelanguages.ui
+
+import androidx.fragment.app.Fragment
+import lt.alexdevstudio.europelanguages.R
+
+class LanguagesFragment : Fragment(R.layout.fragment_languages)
